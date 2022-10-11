87bbaa414edc25d6e2f44b0e0a06842523c1ffb6
Cari Licor <Carilicor@gmail.com>
2022-10-11 16:54:56 -0400

    new edit mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
-import { MongoClient } from "mongodb";
+import { MongoClient,ObjectId } from "mongodb";
 
-import {uri } from "./credentials.js"
+import { uri } from "./credentials.js"
 
 const client = new MongoClient(uri)
 const db = client.db("sample_mflix")
@@ -8,11 +8,14 @@ const db = client.db("sample_mflix")
 const moviesCollections = db.collection("movies")
 //console.log(await moviesCollections.findOne({}) )
 
-let query = {title:{ $regex:/terminator/i}} //serch for "terminator" anywhere in the title and ignore/not case sensitive
-let movieArray = await moviesCollections.find(query).limit(3).toArray()
+let query = { title: { $regex: /star wars/i } } //serch for "terminator" anywhere in the title and ignore/not case sensitive
+let movieArray = await moviesCollections
+    .find(query)
+    //.limit(3)
+    .toArray()//makes it into an array
 
-for(let i=0; i < movieArray.length; i++) {
-    console.log(movieArray[i].title)
+for (let i = 0; i < movieArray.length; i++) {
+    console.log(movieArray[i].title) // how to request one item in an array(multiples)
 }
 
 // let firstMovie = (movieArray[0])
@@ -20,4 +23,24 @@ for(let i=0; i < movieArray.length; i++) {
 
 
 
-console.log(`there are ${movieArray.length} movies`)  //does the same as line 9
+//console.log(`there are ${movieArray.length} movies`)  //does the same as line 9
+
+//add a new movie
+
+const newMovie = {
+    title: "The Boca code story",
+    rating: "R",
+    genre: "comedy",
+    releaseDate: "2022/12/16"
+}
+
+//const results = await moviesCollections.insertOne(newMovie)
+//console.log("Results of inserts",results)
+
+
+const updateQuery = { _id: new ObjectId("6345ca5edf027a7ad91e2831") }
+const update = { $set: {title:"The New Boca Code Story"}}
+const results = await moviesCollections.findOneAndUpdate(updateQuery,update);
+console.log(results)
+
+
